9c23123ad1147c3a4da5dfd9508c77e124cb6f50
Logise1 <logise1123@gmail.com>
2026-01-22 22:42:09 +0100

    x

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'madrid2035-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './tools.html',
+    './articles.js',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
